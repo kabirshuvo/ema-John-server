0e63552064013fdcaae2206166fc5990278a9c01
kabirshuvo <kabirshuvo19@gmail.com>
2023-05-16 23:50:50 +0600

    Products route build successfully

--- index.js
+++ index.js
@@ -27,12 +27,15 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
-    const productCollection = client.db('emaJohnDB').collection('products')
+    const productCollection = client.db('emaJhonDB').collection('products');
 
 
     app.get('/products', async(req, res) => {
+
+      console.log('hello form inside')
+
         const result = await productCollection.find().toArray();
         res.send(result)
     })
@@ -45,7 +48,7 @@ async function run() {
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
